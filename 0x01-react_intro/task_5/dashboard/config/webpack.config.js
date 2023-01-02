d81f4f6fd2dfcc9ac500e26e9f371d251d46f6c3
tluciad <tluciad@gmail.com>
2023-01-02 15:34:11 -0500

    Install Babel

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -13,6 +13,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+     },
+      {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
